5147ee3fb82f10eebd68ebc8d0b0c9c9f8d78a0b
mohamedmasood796 <mohamedmasood796@gmail.com>
2023-01-29 20:14:46 +0530

    room select popup page created

--- src/components/user/reserve/Reserve.jsx
+++ src/components/user/reserve/Reserve.jsx
@@ -1,41 +1,48 @@
 import React from 'react'
+import "./reserve.css"
 import { useState } from 'react'
 import useFetch from '../../../hooks/useFetch'
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 
 function Reserve({ setOpen, hotelId }) {
 
-    const [selectedRooms,setSelectedRooms] = useState([])
+    const [selectedRooms, setSelectedRooms] = useState([])
     const { data, loading, error } = useFetch(`http://localhost:5000/api/hotels/room/${hotelId}`)
-    const dates=  useSelector((state)=>state.searchresult.dates) 
+    const dates = useSelector((state) => state.searchresult.dates)
     console.log(dates, 'mubashir kasssssssssssss')
 
-    const getDatesInRange=(startDate,endDate)=>{
+    const getDatesInRange = (startDate, endDate) => {
         const start = new Date(startDate)
         const end = new Date(endDate)
-        const date =new Date(start.getTime())
+        const date = new Date(start.getTime())
 
-        let list=[]
-        while(date<= end){
+        let list = []
+        while (date <= end) {
             list.push(new Date(date).getTime())
-            date.setDate(date.getDate()+1)
+            date.setDate(date.getDate() + 1)
         }
         return list
     }
-    const alldates=getDatesInRange(dates.startDate,dates.endDate)
+    const alldates = getDatesInRange(dates.startDate, dates.endDate)
 
-    const handleSelect=(e)=>{
-        const checked=e.target.checked
-        const value= e.target.value  
-        setSelectedRooms(checked ? [...selectedRooms,value]:selectedRooms.filter((item)=>item!==value ))
+    const isAvailable = (roomNumber) => {
+        const isFound = roomNumber.unavailableDates.some((date) => {
+            alldates.includes(new Date(date).getTime())
+        })
+        return !isFound
     }
-    const handleClick=()=>{
+    const handleSelect = (e) => {
+        const checked = e.target.checked
+        const value = e.target.value
+        setSelectedRooms(checked ? [...selectedRooms, value] : selectedRooms.filter((item) => item !== value))
+    }
+    const handleClick = () => {
 
     }
     return (
         <div className='reserve'>
             <div className="rContainer">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='rClose text-gray-500 w-10 h-10 cursor-pointer absolute top-5 right-5' onClick={() => setOpen(false)}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className='rClose ' onClick={() => setOpen(false)}>
                     <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clipRule="evenodd" />
                 </svg>
                 <span>Select your rooms:</span>
@@ -49,12 +56,14 @@ function Reserve({ setOpen, hotelId }) {
                             </div>
                             <div className="rPrice">{item?.price}</div>
                         </div>
-                        {item?.roomNumbers.map((roomNumber,_id)=>(
-                            <div className="room">
-                                <label>{roomNumber?.number}</label>
-                                <input type="checkbox" value={roomNumber?._id} onChange={ handleSelect} />
-                            </div>
-                        ))}
+                        <div className="rSelectRooms">
+                            {item?.roomNumbers.map((roomNumber, _id) => (
+                                <div className="room">
+                                    <label>{roomNumber?.number}</label>
+                                    <input type="checkbox" value={roomNumber?._id} onChange={handleSelect} disabled={!isAvailable(roomNumber)} />
+                                </div>
+                            ))}
+                        </div>
                     </div>
                 ))}
                 <button onClick={handleClick} className='rButton' >Reserve Now!</button>
