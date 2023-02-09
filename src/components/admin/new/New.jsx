ae2180e2220778970a48363bb76269c30b23ec43
mohamedmasood796 <mohamedmasood796@gmail.com>
2023-02-09 10:12:56 +0530

    signup button set ,work on s3

--- src/components/admin/new/New.jsx
+++ src/components/admin/new/New.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import S3 from "aws-sdk/clients/s3";
 import './new.scss'
 import Sidebar from '../sidebar/Sidebar'
 import Navbar from '../navbar/Navbar'
@@ -7,15 +8,60 @@ import UploadFileIcon from '@mui/icons-material/UploadFile';
 import { addHotel } from '../../../api/adminReq.js'
 
 function New() {
+
+    const S3_BUCKET = process.env.REACT_APP_BUCKETNAME
+    const accessKeyId = process.env.REACT_APP_accessKeyId;
+    const region = process.env.REACT_APP_region
+    const secretAccessKey = process.env.REACT_APP_secretAccessKey;
+    
+    const s3 = new S3({
+        region,
+        accessKeyId,
+        secretAccessKey,
+    });
     const [formData, setFormData] = useState([])
+    const [files, setFile] = useState([]);
+    const [message, setMessage] = useState()
+    const [imageLinks, setImageLinks] = useState([])
     const handleChange = (e) => {
         const { value, name } = e.target
         setFormData({ ...formData, [name]: value })
     }
+    const handleFile = (e) => {
+        let file = e.target.files;
+        for (let i = 0; i < file.length; i++) {
+            const fileType = file[i]["type"];
+            const validImageTypes = ["image/gif", "image/jpeg", "image/png"];
+            if (validImageTypes.includes(fileType)) {
+                setFile([...files, file[i]]);
+            } else {
+                setMessage("only images accepted");
+            }
+        }
+    }
     const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(formData,"formdata")
-        const response = await addHotel(formData)
+        console.log(files);
+        await files.forEach((file) => {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(file);
+            reader.onload = async (e) => {
+                const result = e.target.result;
+                const uploadParams = {
+                    Bucket: S3_BUCKET,
+                    Key: Date.now() + file.name,
+                    Body: result,
+                };
+                await s3.upload(uploadParams)
+                    .promise()
+                    .then((res) => {
+                        console.log(res.Location);
+                        setImageLinks((imageLinks) => [...imageLinks, res.Location]);
+                        console.log(res.Location, "formdata")
+                    });
+            };
+        });
+        // const response = await addHotel(formData)
     }
     return (
         <div className='home'>
@@ -36,7 +82,7 @@ function New() {
                                 <label htmlFor="file">
                                     Image <UploadFileIcon className='icon' />
                                 </label>
-                                <input type="file" style={{ display: 'none' }} id='file' />
+                                <input type="file" onChange={handleFile} style={{ display: 'none' }} id='file' />
                             </div>
                             <div className="formInput">
                                 <label htmlFor="">Hotel name</label>
@@ -45,13 +91,13 @@ function New() {
                             <div className="formInput">
                                 <label htmlFor="">Type</label>
                                 {/* <input type="text" onChange={handleChange} name="type" placeholder='Type' /> */}
-                                <select  className="dropdown">
+                                <select className="dropdown">
                                     <option onChange={handleChange} type="text" name="type">Apartments</option>
                                     <option onChange={handleChange} type="text" name="type"> Hotels</option>
                                     <option onChange={handleChange} type="text" name="type"> Resorts</option>
                                     <option onChange={handleChange} type="text" name="type">villas</option>
                                 </select>
-                              
+
                             </div>
                             <div className="formInput">
                                 <label htmlFor="">City</label>
